39f9fd620416be0e36312f7fb5b143f55d4d4977
Kaito Queiroz <kaito_queiroz@hotmail.com>
2019-01-29 05:54:07 -0200

    Add files via upload

--- js/directives.js
+++ js/directives.js
@@ -68,13 +68,13 @@ function navigationDirective() {
   return directive;
 
   function link(scope, element, attrs) {
-    if(element.hasClass('nav-dropdown-toggle') && angular.element('body').width() > 782) {
+    if(element.hasClass('nav-dropdown-toggle') && angular.element('body')) {
       element.on('click', function(){
         if(!angular.element('body').hasClass('compact-nav')) {
           element.parent().toggleClass('open').find('.open').removeClass('open');
         }
       });
-    } else if (element.hasClass('nav-dropdown-toggle') && angular.element('body').width() < 783) {
+    } else if (element.hasClass('nav-dropdown-toggle')) {
       element.on('click', function(){
         element.parent().toggleClass('open').find('.open').removeClass('open');
       });
