f15dd04098172fa6e01286b1b8b1ca8d2cb93790
Kaito Queiroz <kaito_queiroz@hotmail.com>
2019-02-14 09:55:32 -0200

    Removing Breadcrumbs

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,14 +1,8 @@
 angular
 .module('app')
-.config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', '$breadcrumbProvider', function($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $breadcrumbProvider) {
+.config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', function($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
 
   $urlRouterProvider.otherwise('/');
-
-  $breadcrumbProvider.setOptions({
-    prefixStateName: 'app.main',
-    includeAbstract: true,
-    template: '<li class="breadcrumb-item" ng-repeat="step in steps" ng-class="{active: $last}" ng-switch="$last || !!step.abstract"><a ng-switch-when="false" href="{{step.ncyBreadcrumbLink}}">{{step.ncyBreadcrumbLabel}}</a><span ng-switch-when="true">{{step.ncyBreadcrumbLabel}}</span></li>'
-  });
 
   $stateProvider
   .state('app', {
@@ -22,11 +16,7 @@ angular
     resolve: {
       loadCSS: ['$ocLazyLoad', function($ocLazyLoad) {
         // you can lazy load CSS files
-        return $ocLazyLoad.load([{
-          serie: true,
-          name: 'Flags',
-          files: ['node_modules/flag-icon-css/css/flag-icon.min.css']
-        },{
+        return {
           serie: true,
           name: 'Font Awesome',
           files: ['node_modules/font-awesome/css/font-awesome.min.css']
@@ -43,24 +33,9 @@ angular
     templateUrl: 'views/main.html',
     //page title goes here
     ncyBreadcrumb: {
-      label: 'Home',
+      label: 'New Delivery',
     },
-    //page subtitle goes here
-    params: { subtitle: 'Welcome to ROOT powerfull Bootstrap & AngularJS UI Kit' },
     resolve: {
-      loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {
-        // you can lazy load files for an existing module
-        return $ocLazyLoad.load([
-          {
-            serie: true,
-            name: 'chart.js',
-            files: [
-              'node_modules/chart.js/dist/Chart.min.js',
-              'node_modules/angular-chart.js/dist/angular-chart.min.js'
-            ]
-          },
-        ]);
-      }],
       loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
         // you can lazy load controllers
         return $ocLazyLoad.load({
